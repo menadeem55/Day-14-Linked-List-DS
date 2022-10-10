1504e7dc268b4018b02e2ccfc14e3183fa0873c6
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-10 17:12:57 +0530

    [Added: created linked list]

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -2,8 +2,31 @@ package com.ds;
 
 public class LinkedList {
 
+	Node head;
+
+	class Node {
+
+		Object data;
+		Node ref;
+
+		Node(Object data) {
+
+			this.data = data;
+		}
+	}
+
+	public void elements(Object data) {
+		Node newNode = new Node(data);
+		System.out.println(newNode.data + " ");
+	}
+
 	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+
 		System.out.println("Welcome to LinkedList Programming");
+		list.elements(56);
+		list.elements(30);
+		list.elements(70);
 
 	}
 
