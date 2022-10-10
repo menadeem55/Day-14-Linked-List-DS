d3de08f8ffc7d29e0c19cafc497452de35f5ea82
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-10 22:45:02 +0530

    [Added: delete first element from the Linked List]

--- src/com/ds/LinkedList.java
+++ src/com/ds/LinkedList.java
@@ -1,6 +1,6 @@
 package com.ds;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LinkedList {
 
@@ -70,18 +70,22 @@ public class LinkedList {
 		while (temp != null) {
 
 			if (temp.ref != null)
-				System.out.println(temp.data + " => ");
+				System.out.print(temp.data + " => ");
 			else
 				System.out.println(temp.data);
 			temp = temp.ref;
 		}
 	}
 
+	public void deleteFirst() {
+		head = head.ref;
+	}
+
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 
 		System.out.println("Welcome to LinkedList Programming");
-		System.out.println("Add Nodes at First");
+
 		Scanner sc = new Scanner(System.in);
 
 		list.addFirst(56);
@@ -89,9 +93,14 @@ public class LinkedList {
 		list.addLast(70);
 		list.display();
 
-		System.out.println("Enter position at which you want to add node:~ ");
+		System.out.println("Enter position at which you want to add: ");
 		int position = sc.nextInt();
 		list.addAtPosition(30, position);
+		list.display();
+
+		list.deleteFirst();
+		list.display();
+		list.deleteFirst();
 		list.display();
 
 	}
